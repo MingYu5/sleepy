02595af74f3ff9990bb221870c9e3c36476e5bec
HBWuChang <2243162077@qq.com>
2024-12-12 10:00:54 +0800

    try

--- _example/win.py
+++ _example/win.py
@@ -12,26 +12,30 @@ import requests
 import time
 from pywinauto import Desktop
 while True:
-    windows = Desktop(backend="uia").windows()
-    o = []
-    for w in windows:
-        t = w.window_text().split("-")[-1].strip()
-        if t not in 排除列表 and t not in o and t:
-            o.append(t)
-    print(o)
-    if len(o) > 0:
-        requests.get(
-            服务器地址,
-            params={
-                "pc_status": 0,
-                "pc_app_name": " | ".join(o)
-                + " || "
-                + time.strftime("%Y-%m-%d %H:%M:%S"),
-                "secret": secret,
-            },
-        )
-    else:
-        requests.get(
-            服务器地址, params={"pc_status": 1, "pc_app_name": "", "secret": secret}
-        )
-    time.sleep(10)
+    try:
+        windows = Desktop(backend="uia").windows()
+        o = []
+        for w in windows:
+            t = w.window_text().split("-")[-1].strip()
+            if t not in 排除列表 and t not in o and t:
+                o.append(t)
+        print(o)
+        if len(o) > 0:
+            requests.get(
+                服务器地址,
+                params={
+                    "pc_status": 0,
+                    "pc_app_name": " | ".join(o)
+                    + " || "
+                    + time.strftime("%Y-%m-%d %H:%M:%S"),
+                    "secret": secret,
+                },
+            )
+        else:
+            requests.get(
+                服务器地址, params={"pc_status": 1, "pc_app_name": "", "secret": secret}
+            )
+        time.sleep(10)
+    except Exception as e:
+        if str(e) == "KeyboardInterrupt":
+            break
